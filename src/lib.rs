153cd27a80910c95abd8416522f65aa3d7c46708
Mike Yagley <myagley@gmail.com>
2020-04-11 14:53:58 -0700

    Use compiler path for CC

--- src/lib.rs
+++ src/lib.rs
@@ -254,7 +254,7 @@ impl Build {
         command.args(&["-o", &out.display().to_string()]);
         command.args(self.files.iter());
         command.env("CGO_ENABLED", "1");
-        command.env("CC", ccompiler.cc_env());
+        command.env("CC", ccompiler.path());
         command.env("GOARCH", self.get_goarch()?);
         command.env("GOOS", self.get_goos()?);
 
